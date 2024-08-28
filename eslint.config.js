5b87964c6fb79d74d84d31dab6f2723a84f29a05
Rony <ronybarua-ethical18>
2024-08-28 21:14:44 +0600

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ignorePatterns: ["node_modules/", "build/"],
+    overrides: [
+      {
+        files: ["*.js", "*.ts", "*.tsx"],
+        // other ESLint configurations
+      },
+    ],
+    // other ESLint configurations
+  };
